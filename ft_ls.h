30dd19b4a39b4d71704173307db5487e7bdccb1c
Quentin Backaert <qbackaer@w-r12-p1.s19.be>
2019-01-24 21:01:01 +0100

    started to organize ft_ls.h

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: qbackaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 21:36:16 by qbackaer          #+#    #+#             */
-/*   Updated: 2019/01/24 20:47:26 by qbackaer         ###   ########.fr       */
+/*   Updated: 2019/01/24 21:00:36 by qbackaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,20 +39,27 @@ typedef struct	s_cursors
 	t_entry *wkst_p;
 	t_entry *curs_c;
 	t_entry *curs_p;
-}				t_cursors;
-void			find_weakest_alpha(t_cursors *llc);
-void			init_cursors(t_cursors *llc, t_entry *lst);
-t_entry			*browse_and_display(t_entry *entries_list, char *dirpath, char *options);
-t_entry			*generate_ll(t_entry *entries_list, DIR *dir, char *dirpath, char *options);
-void			ll_display(t_entry *lst_head, char *dirpath, char *options);
-void			ll_read_create(t_entry **entry_list, DIR *dir, char *path, char *options);
+}				t_ces, DIR *dir, char *dirpath, char *options)ursors;
+
+//ft_ls.c
+void			recursive_wpr(t_entry *entry, char *path, char *options);
+int				list(char *dirpath, char *options, int recursive);
+char			*parse_args(int argc, char **argv);
+int				check_opts(char *valid_opt, char *opt_table);
+//list.c
 t_entry			*ll_append_node(t_entry *lst_head, t_entry *newnode);
 t_entry			*ll_append_node_h(t_entry *lst_head, t_entry *newnode);
 t_entry			*ll_create_node(char *path, char *fname);
 t_entry			*ll_add_node(t_entry *lst_head, char *path, char *fname, int where);
 size_t			ll_count_nodes(t_entry *lst);
-int				list(char *dirpath, char *options, int recursive);
-char			*parse_args(int argc, char **argv);
+//list_b.c
+t_entry			*browse_and_display(t_entry *entries_list, char *dirpath, char *options);
+t_entry			*generate_ll(t_entry *entries, DIR *dir, char *dirpath, char *options);
+
+void			find_weakest_alpha(t_cursors *llc);
+void			init_cursors(t_cursors *llc, t_entry *lst);
+void			ll_display(t_entry *lst_head, char *dirpath, char *options);
+void			ll_read_create(t_entry **entry_lisa, DIR *dir, char *path, char *options);
 int				display_entry(char *fname, struct stat *fstat, int l_nmode);
 char			*get_mode(mode_t file_mode);
 char			*get_usrname(uid_t user_id);
@@ -65,6 +72,5 @@ char			*subdir_path(const char *current_path, const char *subdir);
 void			free_list(t_entry *entries);
 t_entry			*ll_alphasort(t_entry *lst, size_t ll_size, char *cwd);
 t_entry			*ll_revrssort(t_entry *lst, char *cwd);
-void			recursive_wpr(t_entry *entry, char *path, char *options);
 
 #endif
